072646e1366e21937a95b9d749b6741659708975
nefertarixvii <delacruzvallesalaska21@gmail.com>
2026-03-08 23:38:51 -0500

    ramitados

--- obj/Debug/net9.0/formularioGitHub.AssemblyInfo.cs
+++ obj/Debug/net9.0/formularioGitHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("formularioGitHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4addaf768822b282ffc588f149121f6069888fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f540f5fe2aecd12d16f98278f11d353af7f43ed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("formularioGitHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("formularioGitHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
