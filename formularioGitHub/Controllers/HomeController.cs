903c692ef605fa9068487d6e7b94c45b6405f5f4
nefertarixvii <145938028+nefertarixvii@users.noreply.github.com>
2026-03-27 21:32:59 -0500

    Actualización final

---
+++ formularioGitHub/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using formularioGitHub.Models;
+
+namespace formularioGitHub.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    // Muestra el formulario
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // Procesa el formulario
+    [HttpPost]
+    public IActionResult Index(string nombre, DateTime fecha, string hobbies, string pago)
+    {
+        if (pago == "no")
+        {
+            ViewBag.Mensaje = "Usted no ha completado su pago.";
+        }
+        else
+        {
+            ViewBag.Mensaje = "Registro completado correctamente.";
+        }
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+        //este es privacy :) :) :))
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
